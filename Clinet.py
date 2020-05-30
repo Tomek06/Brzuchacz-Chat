95b9ca965e38cfefa829630539e1a4e59cfb7f92
Tomek06 <64292029+Tomek06@users.noreply.github.com>
2020-05-30 12:47:00 -0800

    Delete Clinet.py

--- Clinet.py
+++
@@ -1,7 +0,0 @@
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(),2004))
-while True:
-    msg = s.recv(2004)
-    print(msg.decode("utf-8"))
-    while True:
-        s.send(bytes(input(), "utf-8"))
